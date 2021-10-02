94f031900ee3e51d5ebce48c989ea4ac26afe8df
FabioDevCode <0677384849@orange.fr>
2021-10-02 16:44:49 +0200

    Second commit

--- BackP6Api/database/dbConfig.js
+++ BackP6Api/database/dbConfig.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const result = dotenv.config();
 
 mongoose
 
 .connect(
-    "mongodb+srv://FabioP6:azersazers33@cluster0.hgwwt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority/",
+    process.env.HIDE_DB,
     { useNewUrlParser: true, useUnifiedTopology: true },    
 )
 .then(() => console.log("Connxion à MongoDB Réussie"))
